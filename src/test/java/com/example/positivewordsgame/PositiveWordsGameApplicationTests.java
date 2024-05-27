afd3a21a419f49e3e1951736ef52c3fe0fcd20e7
tanyaselvog <tanyaselvog@gmail.com>
2024-05-27 11:31:29 -0700

    Initial commit

---
+++ src/test/java/com/example/positivewordsgame/PositiveWordsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.positivewordsgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositiveWordsGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
