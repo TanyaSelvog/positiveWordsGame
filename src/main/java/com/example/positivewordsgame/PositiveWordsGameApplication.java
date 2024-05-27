d39797ae2651af913f6646700767c3e0fd3dc7d3
tanyaselvog <tanyaselvog@gmail.com>
2024-05-27 11:57:39 -0700

    added to PositiveWordsGameApplication.java

--- src/main/java/com/example/positivewordsgame/PositiveWordsGameApplication.java
+++ src/main/java/com/example/positivewordsgame/PositiveWordsGameApplication.java
@@ -3,11 +3,27 @@ package com.example.positivewordsgame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+
+
 @SpringBootApplication
 public class PositiveWordsGameApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PositiveWordsGameApplication.class, args);
     }
+
+    //Below is from SpringBoot tutorial - just for use as reference
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
 
 }
